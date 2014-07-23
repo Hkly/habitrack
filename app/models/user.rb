df55af580a3dce71f62c878be7c6378bf7d67800
Hannah Yiu <hannahkyiu@gmail.com>
2014-07-23 11:16:37 -0700

    Start building user model for login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,57 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string(255)      not null
+#  email           :string(255)      not null
+#  password_digest :string(255)      not null
+#  session_token   :string(255)      not null
+#  created_at      :datetime
+#  updated_at      :datetime
+#
+
 class User < ActiveRecord::Base
+  attr_reader :password
+
+  before_validation :ensure_session_token
+
+  validates :username, :email, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true}
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    if user.nil? || !is_password(password)
+      return nil
+    end
+    return user
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def is_password?(password)
+      BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    unless password.nil?
+      @password = password
+      self.password_digest = BCrypt::Password.create(password)
+    end
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
 end

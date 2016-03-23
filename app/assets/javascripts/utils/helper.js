10828aff612085b8a8cd613836e1a7dbc27aec08
hkly <hannah@yola.com>
2016-03-23 16:58:36 -0700

    Add a helper for generating a 'random' score

--- app/assets/javascripts/utils/helper.js
+++ app/assets/javascripts/utils/helper.js
@@ -4,9 +4,13 @@ var helpers = HabitrackApp.helpers = {
   makeRandomData: function() {
     var data = [];
     for (var i = 0; i < 12; i++) {
-      data.push(Math.floor(Math.random() * 58) + 40);
+      data.push(this.makeRandomScore());
     }
     data.push(0);
     return data;
+  },
+
+  makeRandomScore: function() {
+    return Math.floor(Math.random() * 58) + 40;
   }
 };
--- app/assets/javascripts/views/friend_list_piece.js
+++ app/assets/javascripts/views/friend_list_piece.js
@@ -2,14 +2,10 @@ HabitrackApp.Views.FriendListPiece = Backbone.View.extend({
   template: JST['sidebar/friend_list_piece'],
   className: "friend-list-piece",
 
-  randomScore: function() {
-    return Math.floor(Math.random() * 58) + 40;
-  },
-
   render: function() {
     var renderedContent = this.template({
       friend: this.model,
-      score: this.randomScore()
+      score: HabitrackApp.helpers.makeRandomScore()
     });
 
     this.$el.html(renderedContent);
